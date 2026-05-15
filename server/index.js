c34e29f8d7d8fb65e9ad2f59222ac79f47c766c3
mattuzuchin <mu26@uw.edu>
2026-05-14 21:52:47 -0700

    added a few files

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 app.use("/tickets", require("./routes/ticketRoutes"));
+app.use("/signin", require("./routes/signin"));
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
--- server/routes/ticketRoutes.js
+++ server/routes/ticketRoutes.js
@@ -2,14 +2,29 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/:userId", (req, res) => {
+router.get("/", (req, res) => {
   const ticketId = Math.floor(Math.random() * 1000000);
   const userId = req.params.userId;
 
   res.json({
-    ticketId,
-    userId
+    ticketId
   });
 });
 
+router.post("/createticket", (req, res) => {
+  const { title, description } = req.body;
+
+  // Validation
+  if (!title || !description) {
+    return res.status(400).json({
+      message: "Title and description are required"
+    });
+  }
+  const ticketId = Math.floor(Math.random() * 1000000);
+
+  res.status(201).json({
+    message: "Ticket created",
+    ticketID: ticketId
+  });
+});
 module.exports = router;
