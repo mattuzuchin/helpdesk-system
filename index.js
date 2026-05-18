4f7cd1c3871fb1c6ca28f9477e65aca1da978dd2
mattuzuchin <mu26@uw.edu>
2026-05-18 11:06:26 -0700

    started login

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use("/tickets", require("./server/routes/ticketRoutes"));
 app.use("/auth", require("./server/routes/authRoutes"));
 app.use("/users", require("./server/routes/userRoutes"));
-
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -17,5 +17,3 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
-
-app.use(express.json());
