6d769a1d828c916d4da874ed774b2864dd2ddcc6
mattuzuchin <mu26@uw.edu>
2026-05-18 10:23:13 -0700

    used cors to allow the port from frontend to use the backend ports due to two separate ports being used

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 app.use(express.json());
 app.use("/tickets", require("./server/routes/ticketRoutes"));
 app.use("/auth", require("./server/routes/authRoutes"));
 app.use("/users", require("./server/routes/userRoutes"));
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -11,3 +17,5 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
+
+app.use(express.json());
