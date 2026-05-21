c12727c2eab34d9c6d86c2dad8ce9e56d677e784
mattuzuchin <mu26@uw.edu>
2026-05-21 14:10:55 -0700

    frontend v1.0 complete for release

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server running on port 3000");
 });
