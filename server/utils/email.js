daeaee78921a9858515e7d855f30511221f002df
mattuzuchin <mu26@uw.edu>
2026-06-14 21:12:09 -0700

    using resend instead of gmail stmp for emails

--- server/utils/email.js
+++ server/utils/email.js
@@ -2,7 +2,7 @@ const { Resend } = require("resend");
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const FROM_EMAIL = "Helpdesk <onboarding@resend.dev>";
+const FROM_EMAIL = `onboarding@resend.dev`;
 
 const sendPasswordResetEmail = async (toEmail, resetToken) => {
     const resetLink = `https://helpdesk-frontend-sjje.vercel.app/resetPassword?token=${resetToken}`;
